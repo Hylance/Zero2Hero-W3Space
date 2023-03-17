eeaa943306b3f960ae7b1390b1741f977f50c384
cuegod <hengxingtianxia@gmail.com>
2023-03-17 15:02:28 +0800

    add support for webrtc

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,25 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+Vue.use(VueRouter);
 
-export default router
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  }
+  // {
+  //   path: "/1v1",
+  //   name: "1v1",
+  //   component: () => import("../views/1v1.vue")
+  // }
+];
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
+
+export default router;
